638d6ad9e9a07eb896578944d76ee276ee45c290
Alejandro Luevano <64499571+Giold@users.noreply.github.com>
2025-02-11 21:17:06 -0600

    Avances

--- EmployePortal/Controllers/EmployeesController.cs
+++ EmployePortal/Controllers/EmployeesController.cs
@@ -1,6 +1,9 @@
 ﻿using EmployePortal.Datos;
+using EmployePortal.Modelos;
+using EmployePortal.Modelos.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployePortal.Controllers
 {
@@ -21,11 +24,71 @@ namespace EmployePortal.Controllers
             return Ok(contextoDB.Employees.ToList());
         }
 
-        [HttpPut]
-        public IActionResult AddEmployee()
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeId(Guid id) 
         {
+            var employee = contextoDB.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
 
+            return Ok(employee);
+        }
+        
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            contextoDB.Employees.Add(employeeEntity);
+            contextoDB.SaveChanges();
+
+            return Ok(employeeEntity);
 
         }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = contextoDB.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            contextoDB.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = contextoDB.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            contextoDB.Employees.Remove(employee);
+            contextoDB.SaveChanges();
+            return Ok();
+        }
+
     }
 }
